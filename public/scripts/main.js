cd73bc86f7e723bce794e4c407360bd59cc9fb0e
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-08-27 12:41:50 -0300

    Updated Modal Form

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -18,7 +18,15 @@ deleteButtons.forEach(button => {
 })
 
 function handleClick(event, check = true) {
+  event.preventDefault()
   const text = check ? 'Marcar como lida' : 'Excluir'
+  const slug = check ? 'check' : 'delete'
+  const roomId = document.querySelector('#room-id').dataset.id
+  const questionId = event.target.dataset.question
+
+  const form = document.querySelector('.modal form')
+  form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+
   modalTitle.innerHTML = `${text} esta pergunta`
   modalDescription.innerHTML = `Tem certeza, que você deseja ${text.toLocaleLowerCase()} esta pergunta?`
   modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
