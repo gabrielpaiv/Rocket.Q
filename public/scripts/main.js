f8b6326abaccb0d5155e74c038ac8cc2cb2426eb
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-08-27 12:29:20 -0300

    Updated Buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,14 +9,19 @@ const modalButton = document.querySelector('.modal button')
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
 const deleteButtons = document.querySelectorAll('.actions a.delete')
 
 deleteButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    modal.open()
-  })
+  button.addEventListener('click', event => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+  const text = check ? 'Marcar como lida' : 'Excluir'
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Tem certeza, que você deseja ${text.toLocaleLowerCase()} esta pergunta?`
+  modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+  modal.open()
+}
